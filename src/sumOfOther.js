c866693db9438fa38dd3e13b1c7350f829916241
Sergey <gck.sergey@gmail.com>
2018-10-28 18:03:38 +0300

    style: correct code with eslint

--- src/sumOfOther.js
+++ src/sumOfOther.js
@@ -1,4 +1,4 @@
-module.exports = function sumOfOther(arr){
-	let sum = arr.reduce((pv,nv)=>pv+nv);
-	return arr.map(i=>sum-i)	
-}
+module.exports = function sumOfOther(arr) {
+  const sum = arr.reduce((pv, nv) => pv + nv);
+  return arr.map(i => sum - i); // says error " Expected linebreaks to be 'LF' but found 'CRLF'", i dont know how to fix that
+};
